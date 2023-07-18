ae1dd744d89cdf0c35b81bcfa5c4b29b860c969f
Ivanna86Koval <ms.ivanna86@gmail.com>
2023-07-18 19:38:08 +0300

    summary

---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+/*import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';*/
+
+const refs = {
+  inputDatetime: document.querySelector('#datetime-picker'),
+  buttonStart: document.querySelector('[data-start]'),
+  dataDays: document.querySelector('[data-days]'),
+  dataHours: document.querySelector('[data-hours]'),
+  dataMinutes: document.querySelector('[data-minutes]'),
+  dataSeconds: document.querySelector('[data-seconds]'),
+};
